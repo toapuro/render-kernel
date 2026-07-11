409137e12d213b36f82c48eae46c54a4e98d19a0
toapuro <164835903+toapuro@users.noreply.github.com>
2026-07-11 12:08:26 +0900

    fix: 不要なtarget範囲チェックの削除

--- src/main/java/io/github/toapuro/renderkernel/api/gl/state/GlStateTracker.java
+++ src/main/java/io/github/toapuro/renderkernel/api/gl/state/GlStateTracker.java
@@ -1,6 +1,5 @@
 package io.github.toapuro.renderkernel.api.gl.state;
 
-import com.google.common.base.Preconditions;
 import it.unimi.dsi.fastutil.ints.Int2IntMap;
 import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
 import org.lwjgl.opengl.GL15;
@@ -15,7 +14,6 @@ public final class GlStateTracker {
     }
 
     public void ensureBufferBound(int target, int bufferId) {
-        Preconditions.checkArgument(GL15.GL_STREAM_DRAW <= target && target <= GL15.GL_DYNAMIC_COPY, "target is out of range");
         if (boundBuffers.put(target, bufferId) != bufferId) {
             GL15.glBindBuffer(target, bufferId);
         }
@@ -28,6 +26,7 @@ public final class GlStateTracker {
     public void ensureVertexArrayBound(int vertexArray) {
         if (boundVertexArray != vertexArray) {
             GL30.glBindVertexArray(vertexArray);
+            boundVertexArray = vertexArray;
         }
     }
 
